4131f86fc9fdf9bdd8d3a3cf487424f647dca0e1
Cydonia01 <malizdmr011@gmail.com>
2026-03-29 21:33:02 +0300

    fix: changed port 5173 to port 3000

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 CORS_ALLOWED_ORIGINS = env.list(
     "CORS_ALLOWED_ORIGINS",
-    default=["http://localhost:5173", "http://127.0.0.1:5173"],
+    default=["http://localhost:3000", "http://127.0.0.1:3000"],
 )
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = env.list(
     "CSRF_TRUSTED_ORIGINS",
-    default=["http://localhost:5173", "http://127.0.0.1:5173"],
+    default=["http://localhost:3000", "http://127.0.0.1:3000"],
 )
 
 # Application definition
