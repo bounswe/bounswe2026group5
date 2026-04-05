90fd8b9b528e98c9832cca202b5c48b21f2d1084
I-Kazanci <inankazanci@gmail.com>
2026-04-05 15:50:12 +0300

    refactor: typo.

--- web/src/routes/_authorized/discover.tsx
+++ web/src/routes/_authorized/discover.tsx
@@ -9,7 +9,7 @@ import { DiscoverSearchBar } from '@/components/features/discover/DiscoverSearch
 import { DiscoverFilterPanel } from '@/components/features/discover/DiscoverFilterPanel'
 import { mentorSearchInfiniteQueryOptions, allSkillsQueryOptions } from '@/lib/queries/DiscoverQueries.ts'
 
-const PAGE_SIZE = 3
+const PAGE_SIZE = 6
 
 export const Route = createFileRoute('/_authorized/discover')({
     component: DiscoverPage,
