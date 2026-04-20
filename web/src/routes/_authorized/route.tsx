12a6ebae818e8c567cfe3e56c04e125b4f29c010
I-Kazanci <inankazanci@gmail.com>
2026-04-20 15:43:45 +0300

    feat: Added messaging page, and also connected it with api.

--- web/src/routes/_authorized/route.tsx
+++ web/src/routes/_authorized/route.tsx
@@ -17,7 +17,6 @@ function AuthorizedLayout() {
   return (
     <div className="flex min-h-screen flex-col bg-black/[0.02] dark:bg-background">
       <AuthorizedHeader />
-      
       <main className="flex-1">
         <Outlet />
       </main>
